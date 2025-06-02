937266a8d87d66bcbf3f187d2cfc6e5005746681
Ngô Việt Hoài Bảo <baonvh@phenikaa-x.com>
2025-06-02 16:59:50 +0700

    fix year timestamp

--- UdpParser/src/GeneralParser.cpp
+++ UdpParser/src/GeneralParser.cpp
@@ -166,6 +166,9 @@ int64_t GeneralParser::GetMicroLidarTimeU64(const uint8_t* utc, int size, uint32
     if (t.tm_year >= 200) {
       t.tm_year -= 100;
     }
+    else if (t.tm_year <= 100){
+      t.tm_year += 100;
+    }
     t.tm_mon = utc[1] - 1;
     t.tm_mday = utc[2];
     t.tm_hour = utc[3];
